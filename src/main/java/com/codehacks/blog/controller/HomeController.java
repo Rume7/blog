aac7007e88528318f3fb151dd984644c11f4a180
Rume7 <electengrhume@gmail.com>
2021-09-09 11:54:41 +0100

    Fixed login and logout in the application

--- src/main/java/com/codehacks/blog/controller/HomeController.java
+++ src/main/java/com/codehacks/blog/controller/HomeController.java
@@ -11,8 +11,13 @@ import java.util.List;
 import java.util.Optional;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.security.enterprise.SecurityContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 
 @RequestScoped
 @Named
@@ -21,12 +26,18 @@ public class HomeController {
     @Inject
     DataService dataService;
     
+    @Inject
+    SecurityContext securityContext;
+    
+    @Inject
+    FacesContext facesContext;
+    
     private Optional<User> currentUser;
     private List<Quality> currentQualities;
     
     @PostConstruct
     public void initialize() {
-        String username = "Tom";
+        String username = securityContext.getCallerPrincipal().getName();
         this.currentUser = dataService.getUser(username);
         this.currentUser.ifPresent(user -> {
             this.currentQualities = dataService.getQualities(user);
@@ -39,5 +50,11 @@ public class HomeController {
 
     public List<Quality> getCurrentQualities() {
         return currentQualities;
+    }
+    
+    public String logout() throws ServletException {
+        ExternalContext ec = facesContext.getExternalContext();
+        ((HttpServletRequest)ec.getRequest()).logout();
+        return "/login.xhtml?faces-redirect=true";
     }
 }
